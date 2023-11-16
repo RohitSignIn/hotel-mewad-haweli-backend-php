d40ec08b93aa3758dac22e86bb0cd1ac4b9b680a
RohitSignIn <rohitsinghsignin@gmail.com>
2023-11-16 09:17:29 +0530

    Create POST req for room booking, created entry in mysql DB

--- APIs/RoomBooking.php
+++ APIs/RoomBooking.php
@@ -1,28 +1,43 @@
 <?php 
+include("../config/db.php");
 
+
+// Create Room Booking
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try{
 
         $name = $_POST["name"];
         $mobile = $_POST["mobile"]; 
         $email = $_POST["email"];
-        $bookedStr = $_POST["bookedStr"];
+        $booked_str = $_POST["booked_str"];
         
-        $data = [
-            'name' => $name,
-            'mobile' => $mobile,
-            'email' => $email,
-            'booked_str' => $bookedStr,
-            ]
-            
-            $sql = "INSERT INTO users (name, surname, sex) VALUES (:name, :surname, :sex)";
-            $pdo->prepare($sql)->execute($data);
-
-            http_response_code(201);
-            return header("Created Successfully");
+        if($name && $mobile && $email && $booked_str){
+            $data = [
+                'name' => $name,
+                'mobile' => $mobile,
+                'email' => $email,
+                'booked_str' => $booked_str,
+            ];
+                
+                $sql = "INSERT INTO booked_rooms (name, mobile, email, booked_str) VALUES (:name, :mobile, :email, :booked_str)";
+                $pdo->prepare($sql)->execute($data);
+                
+                // Response if created successfully
+                header('Content-type: application/json');
+                http_response_code(201);
+                echo json_encode([
+                    'message' => 'Successfully Created',
+                ]);
+        }else{
+            throw new Exception("Invalid Data, All fields are required");
+        }
         }catch(Exception $e){
-            $pdo->rollback();
-            throw $e;
+            // Response if created successfully
+            header('Content-type: application/json');
+            http_response_code(500);
+            echo json_encode([
+                'message' => $e,
+            ]);
         }
     }
 
