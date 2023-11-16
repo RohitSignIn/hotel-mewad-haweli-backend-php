212da64759ae5d12919696225b95ba8b49537420
RohitSignIn <rohitsinghsignin@gmail.com>
2023-11-16 10:10:54 +0530

    Some changes in POST req, working on DELETE Req

--- APIs/RoomBooking.php
+++ APIs/RoomBooking.php
@@ -32,11 +32,49 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 // Create Room Booking
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try{
+        if(isset($_POST["name"]) && isset($_POST["mobile"]) && isset($_POST["email"]) && isset($_POST["booked_str"])){
 
         $name = $_POST["name"];
         $mobile = $_POST["mobile"]; 
         $email = $_POST["email"];
         $booked_str = $_POST["booked_str"];
+        
+            $data = [
+                'name' => $name,
+                'mobile' => $mobile,
+                'email' => $email,
+                'booked_str' => $booked_str,
+            ];
+                
+                $sql = "INSERT INTO booked_rooms (name, mobile, email, booked_str) VALUES (:name, :mobile, :email, :booked_str)";
+                $pdo->prepare($sql)->execute($data);
+                $id = $pdo->lastInsertId();
+
+                // Response if created successfully
+                header('Content-type: application/json');
+                http_response_code(201);
+                echo json_encode([
+                    'message' => 'Successfully Created',
+                    'data' => array('id' => $id) 
+                ]);
+        }else{
+            throw new Exception("Invalid Data, All fields are required");
+        }
+        }catch(Exception $e){
+            // Response if created successfully
+            header('Content-type: application/json');
+            http_response_code(500);
+            echo json_encode([
+                'message' => $e,
+            ]);
+        }
+    }
+
+// Delete Booked Room 
+if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
+    try{
+
+        $name = $_POST["name"];
         
         if($name && $mobile && $email && $booked_str){
             $data = [
